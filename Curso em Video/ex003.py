1073c247c602a9a974b326e40c879e74b5a1212a
Matheus Schwenck <schwenckmatheus7@gmail.com>
2025-08-28 19:34:52 -0300

    Update ex003.py

--- Curso em Video/ex003.py
+++ Curso em Video/ex003.py
@@ -1,8 +1,3 @@
-#Pede o primeirio valor para o usuario
 n1 = int(input('Digite um valor: '))
-
-#Pede o segundo valor para o usuario
 n2 = int(input('Digite outro valor: '))
-
-#Mostra na tela os valores e faz a soma dos dois entregando o resultado
 print(f'A soma entre {n1} e {n2} é igual a {n1+n2}')
