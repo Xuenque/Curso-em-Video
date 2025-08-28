22c21e6b02d4c166979d18e998d6281abaabfeb2
Matheus Schwenck <schwenckmatheus7@gmail.com>
2025-08-28 19:35:07 -0300

    Update ex002.py

--- Curso em Video/ex002.py
+++ Curso em Video/ex002.py
@@ -1,8 +1,2 @@
-#Pede o nome do usuario
 nome = str(input('Digite seu nome: ')).title().strip()
-
-#Mostra na tela uma mensagem mais o nome do usuario
 print(f'É um prazer em te conhecer, {nome}!')
-
-#.strip() -> tira os espaços do inicio da frase, caso apertem espaço antes de digitar o nome
-#.title() -> deixa as primeiras letras de todas palavras em maiusculos
