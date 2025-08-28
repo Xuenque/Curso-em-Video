29a84075f5fd82d801333e104acdc6d7f890e296
Matheus Schwenck <schwenckmatheus7@gmail.com>
2025-08-28 19:36:14 -0300

    Update ex006.py

--- Curso em Video/ex006.py
+++ Curso em Video/ex006.py
@@ -1,2 +1,3 @@
 num = int(input('Digite um número: '))
 print(f'O dobro de {num} é: {num*2} \nO triplo de {num} é: {num*3} \nE a raiz quadrada de {num} é: {num**(1/2):.2f}')
+#Finalizado com sucesso!
