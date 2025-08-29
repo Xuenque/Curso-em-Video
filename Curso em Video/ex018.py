1d576be5987badbb8af32a90f068bd4ca3234c5b
Matheus Schwenck <schwenckmatheus7@gmail.com>
2025-08-29 11:19:27 -0300

    Add files via upload

---
+++ Curso em Video/ex018.py
@@ -0,0 +1,6 @@
+from math import cos, sin, tan, radians
+angulo = float(input('Digite o angulo: '))
+print(f'O ângulo de {angulo} tem o SENO de {sin(radians(angulo)):.2f}'
+      f'\nO ângulo de {angulo} tem o COSSENO de {cos(radians(angulo)):.2f}'
+      f'\nO ângulo de {angulo} tem a TANGENTE de {tan(radians(angulo)):.2f}')
+#Finalizado com sucesso!
