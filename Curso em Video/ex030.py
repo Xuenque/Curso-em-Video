45744ff8c4f2ce3dddb12ab8d391aced08cb95be
Matheus Schwenck <schwenckmatheus7@gmail.com>
2025-09-01 17:34:58 -0300

    Add files via upload

---
+++ Curso em Video/ex030.py
@@ -0,0 +1,6 @@
+num = int(input('\033[35mMe diga um número qualquer:\033[m '))
+if num % 2 == 0:
+    print(f'O número {num} é \033[36mPAR.\033[m')
+else:
+    print(f'O número {num} é \033[33mIMPAR\033[m')
+#Finalizado com sucesso!
