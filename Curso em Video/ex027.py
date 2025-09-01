6eb72e23f6c68622de0c95a1270bb548efb6c2b5
Matheus Schwenck <schwenckmatheus7@gmail.com>
2025-09-01 17:34:16 -0300

    Add files via upload

---
+++ Curso em Video/ex027.py
@@ -0,0 +1,5 @@
+nome = str(input('Digite o seu nome completo: ')).strip().title().split()
+print(f'Muito prazer em te conhecer!'
+      f'\nSeu primeiro nome é {nome[0]}'
+      f'\nSeu ultimo nome é {nome[-1]}')
+#Finalizado com sucesso!
