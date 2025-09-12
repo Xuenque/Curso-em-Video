95c766d77fb1dd1caf5474a8f832b0858cdf430e
Matheus Schwenck <schwenckmatheus7@gmail.com>
2025-09-12 10:28:36 -0300

    Update ex052.py

--- Curso em Video/ex052.py
+++ Curso em Video/ex052.py
@@ -1,5 +1,5 @@
-num = int(inptu('Digite um número: ')
-for i in range(num)
+num = int(input('Digite um número: ')
+for i in range(num):
     if num % i == 0:
         print('\033[33m', end='')
     else:
