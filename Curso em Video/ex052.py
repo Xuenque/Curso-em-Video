1df1281330d74783363e86adca5433bb47e1095f
Matheus Schwenck <schwenckmatheus7@gmail.com>
2025-09-14 17:02:30 -0300

    Add files via upload

---
+++ Curso em Video/ex052.py
@@ -0,0 +1,14 @@
+cont = 0
+num = int(input('Digite um número: '))
+for i in range(1, num + 1):
+    if num % i == 0:
+        print('\033[33m', end=' ')
+        cont += 1
+    else:
+        print('\033[31m', end=' ')
+    print(i, end='')
+print('')
+if cont == 2:
+    print(f'\n\033[mO número {num} foi divisível {cont} vezes \nE por isso ele É PRIMO!')
+else:
+    print(f'\n\033[mO número {num} foi divisível {cont} vezes \nE por isso ele NÃO É PRIMO!')
