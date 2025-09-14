77c04d046ce327d89274a46d31317d153400ec82
Matheus Schwenck <schwenckmatheus7@gmail.com>
2025-09-14 16:02:45 -0300

    Add files via upload

--- Curso em Video/ex052.py
+++
@@ -1,8 +0,0 @@
-num = int(input('Digite um número: ')
-for i in range(num):
-    if num % i == 0:
-        print('\033[33m', end='')
-    else:
-        print('\033[31m', end='')
-
-#Em contrução...
