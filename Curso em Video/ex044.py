5a84a846c6f4befb28e79e7ce7bef0761967337e
Matheus Schwenck <schwenckmatheus7@gmail.com>
2025-09-14 12:33:03 -0300

    Update ex044.py

--- Curso em Video/ex044.py
+++ Curso em Video/ex044.py
@@ -19,3 +19,4 @@ elif pagamento == 4:
         print(f'O preço do produto de R$ {preco:.2f} em {vezes}x de R$ {(preco + (preco * 20 / 100)) / vezes:.2f} \033[31mCOM JUROS\033[m no cartão sai por R$ {preco + (preco * 20 / 100):.2f}')
 else:
     print('\033[31mOpção invalida\033[m')
+#Finalizado com sucesso!
