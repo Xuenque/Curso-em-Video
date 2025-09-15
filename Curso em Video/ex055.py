ccf4001791f7a920b11140dc997645e92f2a7875
Matheus Schwenck <schwenckmatheus7@gmail.com>
2025-09-15 09:51:41 -0300

    Update ex055.py

--- Curso em Video/ex055.py
+++ Curso em Video/ex055.py
@@ -1,18 +1,14 @@
 maior = 0
 menor = 0
-
 for i in range(5):
     peso = float(input(f'Peso da {i + 1}° pessoa (kg): '))
-
     if i == 1:
         maior = peso
         menor = peso
-
     else:
         if peso > maior:
             maior = peso
         if peso < menor:
             menor = peso
-
 print(f'O maior peso é de {maior} \nO menor peso é de {menor}')
 #Finalizado com sucesso!
