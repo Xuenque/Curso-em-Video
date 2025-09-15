c250a7a9ec0d8782eac8a9e7501b1a826b785b8b
Matheus Schwenck <schwenckmatheus7@gmail.com>
2025-09-15 07:57:21 -0300

    Atualizar o ex056.py

--- Curso em Video/ex056.py
+++ Curso em Video/ex056.py
@@ -11,14 +11,18 @@ for i in range(4):
     idade = int(input('Idade: '))
     idades.append(idade)
     sexo = str(input('Sexo [M/F]: ')).strip()
+
     if i == 0 and sexo in 'Mm':
         maior_idade = idade
         homem_velho = nome
+    
     if sexo in 'Mm' and idade > maior_idade:
         maior_idade = idade
         homem_velho = nome
+    
     if sexo in 'Ff' and idade > 20:
         mulher_20 += 1
+
 print('\nAnalisando...')
 sleep(1)
 print(f'A média de idade do grupo é de {(sum(idades)/4)} anos')
