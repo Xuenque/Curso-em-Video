07ebea6ac42ffd267a570987d5998d96acdc6ad7
Matheus Schwenck <schwenckmatheus7@gmail.com>
2025-09-15 09:51:16 -0300

    Update ex053.py

--- Curso em Video/ex053.py
+++ Curso em Video/ex053.py
@@ -2,10 +2,8 @@ frase = str(input('Digite uma frase: ')).strip().upper()
 palavras = frase.split()
 junto = ''.join(palavras)
 inverso =''
-
 for letra in range(len(junto) -1, -1, -1):
     inverso += junto[letra]
-
 print(f'O inverso de {frase} é {inverso}')
 if inverso == junto:
     print('A frase digitada é um palíndromo!')
