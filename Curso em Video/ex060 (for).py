f4a4740d92a5146eacb60f6e2aff8766e4070568
Matheus Schwenck <schwenckmatheus7@gmail.com>
2026-05-19 09:48:20 -0300

    Update ex060 (for).py

--- Curso em Video/ex060 (for).py
+++ Curso em Video/ex060 (for).py
@@ -2,9 +2,12 @@ num = int(input('Digite o seu número: '))
 fatorial = 1
 for fato in range (num, 0, -1):
     fatorial = fatorial * fato
+    
     if fato > 1:
         print(f'{fato}', end=' x ')
+        
     else:
         print(f'{fato}', end=' = ')
+        
 print(f'{fatorial}')
 #Finalizado com sucesso!
