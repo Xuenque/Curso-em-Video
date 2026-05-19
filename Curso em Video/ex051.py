4a1b22e02ee020fed208f370fe40af32212f9a6d
Matheus Schwenck <schwenckmatheus7@gmail.com>
2026-05-19 09:45:43 -0300

    Update ex051.py

--- Curso em Video/ex051.py
+++ Curso em Video/ex051.py
@@ -1,10 +1,13 @@
 print('=' * 28)
 print(' ' * 4, '10 TERMOS DE UM PA')
 print('=' * 28)
+
 pt = int(input('Primeiro Termo: '))
 r= int(input('Razão: '))
 decimo = pt + (10 - 1) * r
+
 for i in range(pt, decimo + r, r):
     print(i,end=' -> ')
+    
 print('ACABOU')
 #Finalizado com sucesso!
