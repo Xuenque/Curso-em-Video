04e4014367da11bb2816df3ef6ab7ae08677e32a
Matheus Schwenck <schwenckmatheus7@gmail.com>
2026-05-19 09:42:31 -0300

    Update ex044.py

--- Curso em Video/ex044.py
+++ Curso em Video/ex044.py
@@ -13,10 +13,13 @@ elif pagamento == 3:
 
 elif pagamento == 4:
     vezes = int(input('Quantas vezes? \n'))
+  
     if vezes < 3:
         print(f'O produto de R$ {preco:.2f} em 2x no cartão fica R$ {preco / vezes:.2f}')
+      
     elif vezes >= 3:
         print(f'O preço do produto de R$ {preco:.2f} em {vezes}x de R$ {(preco + (preco * 20 / 100)) / vezes:.2f} \033[31mCOM JUROS\033[m no cartão sai por R$ {preco + (preco * 20 / 100):.2f}')
+      
 else:
     print('\033[31mOpção invalida\033[m')
 #Finalizado com sucesso!
