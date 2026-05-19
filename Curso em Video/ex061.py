d7586c2b9cb27739612ff8cda99354d42e28650c
Matheus Schwenck <schwenckmatheus7@gmail.com>
2026-05-19 09:49:14 -0300

    Update ex061.py

--- Curso em Video/ex061.py
+++ Curso em Video/ex061.py
@@ -1,9 +1,11 @@
 print('=' * 28)
 print(' ' * 4, '10 TERMOS DE UM PA')
 print('=' * 28)
+
 pt = int(input('Digite o primeiro termo: '))
 r = int(input('Digite a razão: '))
 cont = 1
+
 while cont < 11:
     print(f'{pt}', end=' ')
     pt += r
