3ffaacab24ce8ef2db9fda7fb784089fc5002ad9
Matheus Schwenck <schwenckmatheus7@gmail.com>
2026-05-19 09:48:36 -0300

    Update ex060 (while).py

--- Curso em Video/ex060 (while).py
+++ Curso em Video/ex060 (while).py
@@ -1,11 +1,15 @@
 num = int(input('Digite o seu número: '))
 fatorial = 1
+
 while num > 0:
     fatorial = fatorial * num
     num -= 1
+    
     if num > 0:
         print(f'{num + 1} x ', end='')
+        
     else:
         print(f'{num + 1} = ', end='')
+        
 print(f'{fatorial}')
 #Finalizado com sucesso!
