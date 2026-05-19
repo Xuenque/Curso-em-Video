9cff5bb9604d1bd3f6d02ff66689dc1200f0eafd
Matheus Schwenck <schwenckmatheus7@gmail.com>
2026-05-19 09:47:44 -0300

    Improve input validation for gender selection
    Add validation for user input on gender selection.

--- Curso em Video/ex057.py
+++ Curso em Video/ex057.py
@@ -1,7 +1,9 @@
 sexo = ''
 while sexo not in ['F', 'M']:
     sexo = str(input('Qual o seu sexo? [F/M] ')).upper()
+    
     if sexo not in ['F', 'M']:
         print('Opção invalida, digite novamente')
+        
 print('Opção aceita, obrigado por participar da nossa pesquisa')
 #Finalizado com sucesso!
