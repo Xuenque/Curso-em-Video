7f056be8dbef5d1418c675aae23bdb4c1ec46afb
Matheus Schwenck <schwenckmatheus7@gmail.com>
2026-05-19 14:38:09 -0300

    Update ex057.py

--- Curso em Video/ex057.py
+++ Curso em Video/ex057.py
@@ -1,6 +1,6 @@
 sexo = ''
 while sexo not in ['F', 'M']:
-    sexo = str(input('Qual o seu sexo? [F/M] ')).upper()
+    sexo = str(input('Iforme seu sexo: [F/M] ')).upper()
     
     if sexo not in ['F', 'M']:
         print('Opção invalida, digite novamente')
